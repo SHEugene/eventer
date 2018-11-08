fddc10bfc8cd774618af13b6b0c6c7a93d239b47
Eugene <eugene.shulga@dbbsoftware.com>
2018-11-08 19:44:25 +0200

    added base for models and connecting to db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('config');
 const app = express();
-
+require('./app/models');
 const port = config.get('defaultAddress.port');
 
 app.listen(port, () => {
