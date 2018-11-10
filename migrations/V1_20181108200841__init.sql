8dea76fc494bc04f739d23b36308d16a9a2fc632
Eugene <eugene.shulga@dbbsoftware.com>
2018-11-10 13:33:51 +0200

    added user  to database

--- migrations/V1_20181108200841__init.sql
+++ migrations/V1_20181108200841__init.sql
@@ -1,5 +1,13 @@
 CREATE TABLE `User` (
 	`id` int (11) NOT NULL AUTO_INCREMENT,
 	`name` varchar(255) NOT NULL,
+	`surname` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255) DEFAULT NULL,
+	`city` varchar(255) DEFAULT NULL,
+	`profilePhotoS3Key` varchar(255) DEFAULT NULL,
+	`country` varchar(255) DEFAULT NULL,
+	`DOB` date DEFAULT NULL,
+	`authorisationType` enum('EMAIL','GOOGLE') DEFAULT 'EMAIL',
 	PRIMARY KEY (`id`)
 );
