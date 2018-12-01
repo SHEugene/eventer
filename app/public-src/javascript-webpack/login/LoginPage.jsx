82c21ce7a68dd6353b0f0121256406f9e20c3463
Eugene <eugene.shulga@dbbsoftware.com>
2018-12-01 11:10:34 +0200

    added bootstrap to the project

--- app/public-src/javascript-webpack/login/LoginPage.jsx
+++ app/public-src/javascript-webpack/login/LoginPage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import {connect} from 'react-redux';
-
+import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
 class LoginPage extends  React.Component {
 	constructor (props) {
 		super(props);
 	}
 	render() {
 		return (
-			<div className='red' >
-				Hello
-			</div>
+			<form className='container'>
+				<FormGroup className='row'>
+					<ControlLabel className='col-lg-5'>Email</ControlLabel>
+					<FormControl type='input' placeholder='Enter your email '/>
+				</FormGroup>
+			</form>
 		);
 	}
 }
